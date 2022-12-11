4cdd41f45b2ff8aab1d9f90e03965843b144b29e
jpcoder12 <117314890+jpcoder12@users.noreply.github.com>
2022-12-11 14:15:44 +0000

    basic form inputs

--- src/components/non-profit-form.js
+++ src/components/non-profit-form.js
@@ -7,11 +7,11 @@ export default function NonProfitForm(){
             <h3>Project Requirements</h3>
             <p>Enter the requirements for the project you are creating for your non-profit organization.</p>
             <form>
-                div
                 <label for="nonprofitName">Name</label>
                 <input name="nonprofitName"/>
                 <label for="projectTitle">Project Title</label>
                 <input name="projectTitle"/>
+                <label for="projectReqs"><h4>Project Requirements</h4></label>
                 <textarea name="projectReqs"/>
             </form>
         </div>
